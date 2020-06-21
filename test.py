49744a7fa3cbc7d2ad00197dc46854f140112e7b
Sonoda Umi <det_conan@live.cn>
2020-06-21 22:30:51 +0900

    Finished the test version

--- test.py
+++ test.py
@@ -21,6 +21,7 @@ if __name__ == "__main__":
     while cap.isOpened():
         ret, frame = cap.read()
         counter += 1
+        counterx = 0
         if ret and counter > 100:
             # img = cv2.resize(frame, None, fx=0.8, fy=0.8)
             img = frame
@@ -28,45 +29,34 @@ if __name__ == "__main__":
             img_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
             y_channel, u_channel, v_channel = cv2.split(img_yuv)
             class_ids, confidences, boxes, indexes = object_detector(net, output_layers, img)
+            # y_channel_lowpassed = low_pass_filtering(y_channel, 200)
+            # b_lowpassed = low_pass_filtering(b, 100)
+            # g_lowpassed = low_pass_filtering(g, 100)
+            # r_lowpassed = low_pass_filtering(r, 100)
+            # restored_image = cv2.merge([b_lowpassed, g_lowpassed, r_lowpassed])
             font = cv2.FONT_HERSHEY_PLAIN
+            frame_objects = []
             for i in range(len(boxes)):
                 if i in indexes:
                     x, y, w, h = boxes[i]
                     label = str(classes[class_ids[i]])
                     color = colors[i]
-                    cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
-                    cv2.putText(img, label, (x, y + 30), font, 1, color, 1)
-            # y_channel_fft = np.fft.fft2(y_channel)
-            # y_channel_fft_spectrum = 20*np.log(np.abs(y_channel_fft))
-            # f_ishift = np.fft.ifftshift(y_channel_fft)
-            # img_back = cv2.idft(f_ishift)
-            y_channel_lowpassed = low_pass_filtering(y_channel, 200)
-            b_lowpassed = low_pass_filtering(b, 100)
-            g_lowpassed = low_pass_filtering(g, 100)
-            r_lowpassed = low_pass_filtering(r, 100)
-            restored_image = cv2.merge([b_lowpassed, g_lowpassed, r_lowpassed])
-            # print(np.max(y_channel_lowpassed))
-            # print(np.min(y_channel_lowpassed))
-            # print(img_yuv)
-            # print((y_channel))
-            # print((y_channel_lowpassed*255).astype(int).shape)
-            # print(np.max(y_channel))
-            # print(np.min(y_channel))
-            # print(y_channel_lowpassed.shape)
-            # print(u_channel.shape)
-            # print(v_channel.shape)
-            # restored_img_yuv = np.dstack([(y_channel_lowpassed).astype(int),u_channel,v_channel])
-            # restored_img_yuv = cv2.merge([(y_channel_lowpassed*255).astype(int),u_channel,v_channel])
-
-            # restored_img = cv2.cvtColor(restored_img_yuv,cv2.COLOR_YUV2BGR)
-            cv2.imshow("Image", restored_image)
-            # cv2.imwrite("output/img%d.png" % counter, y_channel_lowpassed*255)
-            print(b_lowpassed)
-
+                    # cv2.rectangle(img, (x, y), (x + w, y + h), color, 2)
+                    # cv2.putText(img, label, (x, y + 30), font, 1, color, 1)
+                    identified_object = img[y:(y + h), x:(x + w)]
+                    frame_objects.append((identified_object, x, y, w, h, label))
+            blured_background = cv2.GaussianBlur(img, (5, 5), cv2.BORDER_DEFAULT)
+            for item in frame_objects:
+                blured_background[item[2]:(item[2] + item[4]), item[1]:(item[1] + item[3])] = item[0]
+            cv2.imshow("Image", blured_background)
+            cv2.imwrite("test_output/blured%d.png" % counter, blured_background)
+            cv2.imwrite("test_output/original%d.png" % counter, frame)
+            # break
             # print(y_channel)
 
             if cv2.waitKey(25) & 0xFF == ord('q'):
                 break
+
         else:
             pass
             # break
